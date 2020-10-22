44f552f8bd8a2352eddde3dd1e0abccaa6b2731e
t-mesq <t-mesq@users.noreply.github.com>
2020-10-22 19:52:48 +0100

    Already generates TFIDF and boolean

--- project/main.py
+++ project/main.py
@@ -3,39 +3,42 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from pympler import asizeof
 from itertools import groupby
-import xml.etree.ElementTree as ET
-
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 import os
 import time
 import tarfile
-import tqdm
-
+import json
 
 COLLECTION_LEN = 807168
 COLLECTION_PATH = 'collection/'
 DATASET = 'rcv1'
 TRAIN_DATE_SPLIT = '19960930'
-AVAILABLE_DATA = ('newsitem', 'headline', 'text', 'dateline', 'byline')
+AVAILABLE_DATA = ('headline', 'p', 'dateline', 'byline')
 
 
 class InvertedIndex:
-    def __init__(self, preprocessor=None):
-        self.boolean_index = CountVectorizer(input='filename', preprocessor=preprocessor, binary=True)
-        self.tfidf_index = TfidfVectorizer(input='filename', preprocessor=preprocessor)
-        self.tfidf_matrix = None
-        self.boolean_matrix = None
+    def __init__(self, D, preprocessor=None):
+        raw_text_list = self._raw_text_from_dict(D)
+        self.boolean_index = CountVectorizer(preprocessor=preprocessor, binary=True)
+        self.boolean_matrix = self.boolean_index.fit_transform(raw_text_list)
+        self.tfidf_index = TfidfVectorizer(preprocessor=preprocessor, vocabulary=self.boolean_index.vocabulary)
+        self.tfidf_matrix = self.tfidf_index.fit_transform(raw_text_list)
+
+    @staticmethod
+    def _raw_text_from_dict(doc_dict):
+        return [' '.join(list(doc.values())) for docs in doc_dict.values() for doc in docs] #Joins all docs in a single list of raw_doc strings
 
     @property
     def idf(self):
         return self.tfidf_index.idf_
+    @property
+    def vocabulary(self):
+        return self.boolean_index.vocabulary_
 
-    def fit(self, D):
-        document_collection = [D + '/' + filename for filename in os.listdir(D)]
-
-        self.tfidf_matrix = self.tfidf_index.fit_transform(document_collection)
-        self.boolean_matrix = self.boolean_index.fit_transform(document_collection)
-        return self
+    def get_matrix_data(self):
+        return f'{self.idf}\n\n{self.vocabulary}'
 
 
 def indexing(D, preprocess=None, *args):
@@ -43,32 +46,42 @@ def indexing(D, preprocess=None, *args):
     return I.fit(D).idf, start_time - time.time(), asizeof.asizeof(I)
 
 
-def parse_xml_doc(tree):
-    root = tree.getroot()
-    for child in root:
-        print(child.text)
-    return 0
+def parse_xml_doc(filename):
+    parsed_doc = {}
+
+    soup = BeautifulSoup(open(filename).read(), 'lxml')
+
+    for tag in AVAILABLE_DATA:
+        parsed_doc[tag] = ''.join([str(e.string) for e in soup.find_all(tag)])
+
+    return parsed_doc
+
 
 def tqdm_generator(members, n):
-    for member in tqdm.tqdm(members, total=n):
+    for member in tqdm(members, total=n):
         yield member
 
 
 def main():
     if os.path.isdir(COLLECTION_PATH + 'rcv1'):
-        print(f'Directory "{DATASET}" already exists, moving on to indexing.')
+        print(f'Directory "{DATASET}" already exists, moving on to indexing.', flush=True)
     else:
-        print('Directory "rcv1" not found. Extracting "rcv.tar.xz"')
+        print('Directory "rcv1" not found. Extracting "rcv.tar.xz"', flush=True)
         with tarfile.open('collection/rcv1.tar.xz', 'r:xz') as D:
             D.extractall('collection/', members=tqdm_generator(D, COLLECTION_LEN))
 
     train_dirs, test_dirs = [list(items) for key, items in groupby(sorted(os.listdir(COLLECTION_PATH + 'rcv1'))[:-3], lambda x: x == TRAIN_DATE_SPLIT) if not key]
     train_dirs.append(TRAIN_DATE_SPLIT)
 
+    train_docs = {}
     for directory in train_dirs[:1]:
-        for file_name in os.listdir(f"{COLLECTION_PATH}{DATASET}/{directory}")[:1]:
-            print(parse_xml_doc(ET.parse(f"{COLLECTION_PATH}{DATASET}/{directory}/{file_name}")))
+        train_docs[directory] = []
+        for file_name in tqdm(sorted(os.listdir(f"{COLLECTION_PATH}{DATASET}/{directory}"))[:10], desc=f'dir:{directory}'):
+            train_docs[directory].append(parse_xml_doc(f"{COLLECTION_PATH}{DATASET}/{directory}/{file_name}"))
 
+    # print(json.dumps(train_docs, indent=2))
+    I = InvertedIndex(train_docs)
+    print(I.get_matrix_data())
     return 0
 
 
