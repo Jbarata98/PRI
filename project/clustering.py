0fdec02007440e89c7bd1e56e4fe682551d494dd
Carolinav66 <carolina_vitorino@hotmail.com>
2020-11-30 02:34:07 +0000

    nao consegui acabar

--- project/clustering.py
+++ project/clustering.py
@@ -13,9 +13,8 @@ from sklearn.metrics import silhouette_score
 from sklearn.metrics.pairwise import pairwise_distances
 from matplotlib import pyplot as plt
 from sklearn.metrics.cluster import adjusted_rand_score
-from sklearn.metrics.cluster import v_measure_score
-from scipy.cluster.hierarchy import dendrogram
 from scipy.spatial.distance import cdist
+
 
 SUBSET_SIZE = 10000
 RANDOM_STATE = 123
@@ -32,7 +31,6 @@ def checkpoint():
 
 
 # CLUSTERING
-# separar por hierarchical e partitioning
 def clustering(D, approach, distance):
     cp = checkpoint()
     clusters = list([int(1.45 ** i) for i in range(2, 20)])
@@ -52,6 +50,8 @@ def clustering(D, approach, distance):
             model = KMeans(n_clusters=nr, verbose=1, random_state=RANDOM_STATE, n_init=DEFAULT_N_INIT).fit(X)
         elif approach == 'Agglomerative':
             model = AgglomerativeClustering(n_clusters=nr, affinity=distance, linkage="complete").fit(X.toarray())
+        elif approach == 'Ward':
+            model = AgglomerativeClustering(n_clusters=nr, affinity=distance, linkage="ward").fit(X.toarray())
 
         inertias.append(model.inertia_)
 
@@ -86,59 +86,44 @@ def clustering(D, approach, distance):
     plt.title(f"{approach} Distortions Scores showing the optimal k ")
     plt.show()
 
-        # silhouettesaggt.append(silhouette_score(vectorspace_topics, modelagg.labels_, metric=distance))
-        # cluster = clusters[np.argmax(silhouettesaggt)]
-        # print(cluster)
-
-    #
-    #     elif D == docs:
-    #         for i in clusters:
-    #             modelagg = AgglomerativeClustering(n_clusters=i).fit(vectorspace_dtrain.toarray())
-    #             silhouettesaggd.append(silhouette_score(vectorspace_dtrain, modelagg.labels_, metric=distance))
-    #
-    # elif approach == 'K-means':
-    #     if D == topics:
-    #         for i in clusters:
-    #             modelkmeans = KMeans(n_clusters=i).fit(vectorspace_topics.toarray())
-    #             silhouetteskmt.append(silhouette_score(vectorspace_topics, modelkmeans.labels_, metric=distance))
-    #     elif D == docs:
-    #         for i in clusters:
-    #             modelkmeans = KMeans(n_clusters=i).fit(vectorspace_dtrain.toarray())
-    #             silhouetteskmd.append(silhouette_score(vectorspace_dtrain, modelkmeans.labels_, metric=distance))
-
-    # comparing agg with kmeans
+    cluster = clusters[np.argmax(silhouettes)]
+    print(cluster)
+    print((KMeans(n_clusters=cluster).fit(X)).labels_)
+    return ((KMeans(n_clusters=cluster).fit(X)).labels_)
 
 
-#######não funciona - copiado do lab
-def plot_dendrogram(model, **kwargs):
-    # create the counts of samples under each node
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
-    linkage_matrix = np.column_stack([model.children_, model.distances_, counts]).astype(float)
-    dendrogram(linkage_matrix, **kwargs)
-
-
-########################################################
 
 # INTERPRET
 def interpret(cluster, D):
-    # descrever cluster considerando median and medoid criteria
-    # 1º arranjar o cluster
-    return 0
+    # median - relevance of each term for the documents in the cluster
+    for i in range(cluster):
+        np.median(np.array(cluster)[:, i], axis=0)
+
+    #medoid -  object within a cluster for which average dissimilarity between it and all the other the members of the cluster is minimal, most central document in the cluster
+    #nao sei se e para usar o k-medoids
+    #for k in range(cluster):
+    #    medoid_id = np.argmin()
 
 
 # EVALUATE
-# def evaluate(D):
-# @behavior evaluates a solution produced by the introduced clustering function
-# @output clustering internal( and optionally external) criteria
+def evaluate(D, cluster):
+    vector_space = tfidf_vectorizer.fit_transform(' '.join(list(value.values())) for key, value in D.items())
+
+    # Rand Index - computes a similarity measure between two clusterings
+    labels_true = cluster
+    labels_pred = D.target
+    rand_score = adjusted_rand_score(labels_true, labels_pred)
+    print('adjusted rand score = {}'.format(rand_score))
+
+    #Internal Measures:
+    # silhouette coefficient
+    sil_score = silhouette_score(vector_space, cluster, metric='cosine')
+    print('silhouette score = {}'.format(sil_score))
+
+    #Cohesion and Separation
+    #The silhouette value measures how similar a point is to its own cluster(cohesion) compared to other clusters(separation)
+
+    #Faltou me por para aqui uns graficos para o relatorio
 
 
 def main():
@@ -146,7 +131,7 @@ def main():
     docs, topics, topic_index, doc_index = cl.setup()
 
     # agglomerative_clustering(docs, 'Agglomerative', 'cosine')
-    clustering(docs['train'], 'Kmeans', 'cosine')
+    #clustering(docs['train'], 'Kmeans', 'cosine')
     # clustering(topics, 'K-means', 'euclidean')
     return 0
 
